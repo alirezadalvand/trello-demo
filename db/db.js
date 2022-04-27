1c54db298c19eeb3cb9f8c78aa34fa2b7a85ccf8
alirezadalvand <alireza.d1339@gmail.com>
2022-04-27 21:16:38 +0430

    db

---
+++ db/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const connectdb = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected`);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+module.exports = connectdb;
